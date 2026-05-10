4f1cacd88cbeb58a635c7d64ab84744a74b4fae3
x <felixhornby64@outlook.com>
2026-05-10 22:54:08 +0100

    trying to put games in grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ import { join } from "@tauri-apps/api/path";
 import { invoke } from "@tauri-apps/api/core";
 
 let games = [];
+let amountOfGames = 0;
 
 const fileDir = document.querySelector(".settingsBtnSVGLeft");
 
@@ -55,7 +56,23 @@ fileDir.addEventListener("click", async () => {
         };
         console.log(game);
         games.push(game);
+        amountOfGames++;
     }
+    console.log(amountOfGames);
+
+    let gameAmount = amountOfGames;
+    const gameGrids = [gameGridP1, gameGridP2, gameGridP3];
+    const spacesTaken = [12, 12, 12];
+    const maxGames = 36;
+    const takenGames = maxGames - amountOfGames;
+
+    const page1 = 12 - gameAmount;
+    gameAmount - page1;
+    const page2 = 12 - (gameAmount - page1);
+    gameAmount - page2;
+    const page3 = 12 - (gameAmount - page2);
+    gameAmount - page3;
+    console.log(gameAmount, page1, page2, page3);
 });
 
 
@@ -63,95 +80,31 @@ const gameGridP1 = document.querySelector(".gameGrid.P1");
 const gameGridP2 = document.querySelector(".gameGrid.P2");
 const gameGridP3 = document.querySelector(".gameGrid.P3");
 
-function sendNullGame() {
-    return `
-    <div class="game">
-        <svg class="gamePath" viewBox="0 0 200 113" xmlns="http://www.w3.org/2000/svg">
-        
-        <style>
-            .channelWii{
-                fill: #4a4a4a;
-                font-family: "Roboto";
-                font-weight: 650;
-                font-size: 1.75rem;
-                opacity: 5%;
-            }
-        </style>
-        
-        <defs>
+async function makeNullGames() {
+    const res = await fetch("/assets/svgs/nullGame.svg");
+    const svgText = await res.text();
+    const svg = `<div class="game">${svgText}</div>`;
+    
+    const gameGrids = [gameGridP1, gameGridP2, gameGridP3];
+    const spacesTaken = [12, 12, 12];
 
-            <pattern id="lineFill" width="4" height="5" patternUnits="userSpaceOnUse" patternTransform="translate(0,0)">
-                <rect width="4" height="1" fill="rgb(190, 190, 190)" />
-                <rect y="1" width="4" height="4" fill="rgb(167, 167, 167)" />
+    for (let j = 0; j < 3; j++){
+        for (let i = 0; i < spacesTaken[j]; i++){
+            gameGrids[j].style.backgroundColor = "black";
+            spacesTaken[j]--;
+        }
+    }
+    
 
-                <animateTransform
-                    attributeName="patternTransform"
-                    type="translate"
-                    values="0,0; 0,-5;"
-                    dur="1.5s"
-                    repeatCount="indefinite" />
-            </pattern>
-
-            <filter id="tvStatic">
-                <feTurbulence
-                    type="fractalNoise"
-                    baseFrequency="0.5"
-                    numOctaves="1"
-                    seed="2"
-                    stitchTiles="stitch"
-                    result="noise">
-
-                    <animate
-                        attributeName="seed"
-                        values="1;12;24;36;48;60;72;84"
-                        dur="0.18s"
-                        repeatCount="indefinite" 
-                    />
-                </feTurbulence>
-
-                <!-- make it grayscale -->
-                <feColorMatrix in="noise" type="saturate" values="0" result="monoNoise" />
-
-                <!-- crank contrast -->
-                <feComponentTransfer in="monoNoise" result="staticNoise">
-                    <feFuncR type="linear" slope="4" intercept="-1.5" />
-                    <feFuncG type="linear" slope="4" intercept="-1.5" />
-                    <feFuncB type="linear" slope="4" intercept="-1.5" />
-                </feComponentTransfer>
-
-                <!-- keep it only inside the fill shape -->
-                <feComposite in="staticNoise" in2="SourceGraphic" operator="in" result="clippedNoise" />
-
-                <!-- blend into your pattern -->
-                <feBlend in="SourceGraphic" in2="clippedNoise" mode="screen" />
-            </filter>
-        </defs>
-
-            <path transform="translate(5 6)" d="M190,51.5c0-21.61-1.04-39.36-2.36-41.31-2.14-5.92-8.1-10.19-15.14-10.19H17.5C10.46,0,4.5,4.27,2.36,10.19,1.04,12.14,0,29.89,0,51.5s1.04,39.36,2.36,41.31c2.14,5.92,8.1,10.19,15.14,10.19h155c7.04,0,13-4.27,15.14-10.19,1.32-1.96,2.36-19.7,2.36-41.31Z" 
-                fill="url(#lineFill)"
-                filter="url(#tvStatic)"
-                opacity="60%"
-            />
-            <path transform="translate(5 6)" d="M190,51.5c0-21.61-1.04-39.36-2.36-41.31-2.14-5.92-8.1-10.19-15.14-10.19H17.5C10.46,0,4.5,4.27,2.36,10.19,1.04,12.14,0,29.89,0,51.5s1.04,39.36,2.36,41.31c2.14,5.92,8.1,10.19,15.14,10.19h155c7.04,0,13-4.27,15.14-10.19,1.32-1.96,2.36-19.7,2.36-41.31Z"
-                fill="none"
-                stroke="#bbbbbb"
-                stroke-width="2"
-            />
-
-            <text class="channelWii">
-                <tspan x="79" y="69">Wii</tspan>
-            </text>
-
-        </svg>
-    </div>
-    `;
+    for (let j = 0; j < 3; j++){
+        for (let i = 0; i < spacesTaken[j]; i++) {
+            gameGrids[j].insertAdjacentHTML("beforeend", svg);
+        }
+    }
 }
+makeNullGames();
 
-for (let i = 0; i < 12; i++) {
-    gameGridP1.insertAdjacentHTML("beforeend", sendNullGame());
-    gameGridP2.insertAdjacentHTML("beforeend", sendNullGame());
-    gameGridP3.insertAdjacentHTML("beforeend", sendNullGame());
-}
+
 
 const gamesGrid = document.querySelector(".gameGridWrapper");
 const game = document.querySelector(".game");
@@ -180,16 +133,32 @@ function arrowsShow() {
 }
 arrowsShow();
 
-arrowLeft.addEventListener("click", () => {
-    gamesGrid.scrollBy({ left: -(game.clientWidth * 4), behavior: "smooth" });
+function scrollPageToCenter(container, page) {
+    const target =
+        page.offsetLeft -
+        (container.clientWidth / 2) +
+        (page.clientWidth / 2);
 
-    curPage -= 1;
-    arrowsShow();
+    container.scrollTo({
+        left: target,
+        behavior: "smooth"
+    });
+}
+
+const pages = [gameGridP1, gameGridP2, gameGridP3];
+
+arrowLeft.addEventListener("click", () => {
+    if (curPage > 1) {
+        curPage--;
+        scrollPageToCenter(gamesGrid, pages[curPage - 1]);
+        arrowsShow();
+    }
 });
 
 arrowRight.addEventListener("click", () => {
-    gamesGrid.scrollBy({ left: (game.clientWidth * 4), behavior: "smooth" });
-
-    curPage += 1;
-    arrowsShow();
+    if (curPage < pages.length) {
+        curPage++;
+        scrollPageToCenter(gamesGrid, pages[curPage - 1]);
+        arrowsShow();
+    }
 });
